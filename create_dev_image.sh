ccee9e3944cc4053f23e72dfdb7bc544cf5c7f92
Viktor Szakats <commit@vsz.me>
2026-01-24 17:04:06 +0100

    create_dev_image.sh: rename a variable to match sibling scripts (revert)
    After discovering that create_base_image.sh uses both names. Better
    back off from this rename till the meanings are better understood.

--- create_dev_image.sh
+++ create_dev_image.sh
@@ -27,15 +27,15 @@ run_tests=${8}
 # set base and platform
 if [[ -n $platform ]]; then
   echo "creating with platform=${platform}"
-  ctr=$(buildah --platform "${platform}" from "${dist}")
+  bdr=$(buildah --platform "${platform}" from "${dist}")
 else
   echo "creating ..."
-  ctr=$(buildah from "${dist}")
+  bdr=$(buildah from "${dist}")
 fi
 
 # label/env
-buildah config --label maintainer="James Fuller <jim.fuller@webcomposite.com>" "$ctr"
-buildah config --label name="${image_name}" "$ctr"
+buildah config --label maintainer="James Fuller <jim.fuller@webcomposite.com>" "$bdr"
+buildah config --label name="${image_name}" "$bdr"
 
 # determine dist package manager
 if [[ "$dist" =~ .*"alpine".* ]]; then
@@ -52,45 +52,45 @@ if [[ "$dist" =~ .*"debian".* ]]; then
 fi
 
 # install deps using specific dist package manager
-buildah run "$ctr" ${package_manage_update}
-buildah run "$ctr" ${package_manage_add} ${deps}
+buildah run "$bdr" ${package_manage_update}
+buildah run "$bdr" ${package_manage_add} ${deps}
 
 # setup curl source derived from branch or tag
 echo "get curl source"
-buildah run "$ctr" mkdir /src
+buildah run "$bdr" mkdir /src
 if [ "${branch_or_tag:0:4}" = "curl" ]; then
   # its a tag, retrieve release source
-  buildah run "$ctr" /usr/bin/curl -L -o curl.tar.gz "https://github.com/curl/curl/releases/download/${branch_or_tag}/curl-${release_tag}.tar.gz"
-  buildah run "$ctr" tar -xvf curl.tar.gz
-  buildah run "$ctr" rm curl.tar.gz
-  buildah run "$ctr" mv curl-"${release_tag}" /src/curl-"${release_tag}"
-  buildah config --workingdir /src/curl-"${release_tag}" "$ctr"
+  buildah run "$bdr" /usr/bin/curl -L -o curl.tar.gz "https://github.com/curl/curl/releases/download/${branch_or_tag}/curl-${release_tag}.tar.gz"
+  buildah run "$bdr" tar -xvf curl.tar.gz
+  buildah run "$bdr" rm curl.tar.gz
+  buildah run "$bdr" mv curl-"${release_tag}" /src/curl-"${release_tag}"
+  buildah config --workingdir /src/curl-"${release_tag}" "$bdr"
 else
   # its a branch, retrieve archive source
-  buildah run "$ctr" /usr/bin/curl -L -o curl.tar.gz "https://github.com/curl/curl/archive/refs/heads/${branch_or_tag}.tar.gz"
-  buildah run "$ctr" tar -xvf curl.tar.gz
-  buildah run "$ctr" rm curl.tar.gz
-  buildah run "$ctr" mv curl-"${branch_or_tag}" /src/curl-"${branch_or_tag}"
-  buildah config --workingdir /src/curl-"${branch_or_tag}" "$ctr"
+  buildah run "$bdr" /usr/bin/curl -L -o curl.tar.gz "https://github.com/curl/curl/archive/refs/heads/${branch_or_tag}.tar.gz"
+  buildah run "$bdr" tar -xvf curl.tar.gz
+  buildah run "$bdr" rm curl.tar.gz
+  buildah run "$bdr" mv curl-"${branch_or_tag}" /src/curl-"${branch_or_tag}"
+  buildah config --workingdir /src/curl-"${branch_or_tag}" "$bdr"
 fi
 
 # build curl
-buildah run "$ctr" autoreconf -fi
-buildah run "$ctr" ./configure --disable-dependency-tracking ${build_opts}
-buildah run "$ctr" make "-j$(nproc)"
+buildah run "$bdr" autoreconf -fi
+buildah run "$bdr" ./configure --disable-dependency-tracking ${build_opts}
+buildah run "$bdr" make "-j$(nproc)"
 
 # run tests
 if [[ $run_tests -eq 1 ]]; then
-  buildah run "$ctr" make test
+  buildah run "$bdr" make test
 fi
 
 # install curl in /build
-buildah run "$ctr" make DESTDIR="/build/" install "-j$(nproc)"
+buildah run "$bdr" make DESTDIR="/build/" install "-j$(nproc)"
 
 # label image
-buildah config --label org.opencontainers.image.source="https://github.com/curl/curl-container" "$ctr"
-buildah config --label org.opencontainers.image.description="minimal dev image for curl" "$ctr"
-buildah config --label org.opencontainers.image.licenses="MIT" "$ctr"
+buildah config --label org.opencontainers.image.source="https://github.com/curl/curl-container" "$bdr"
+buildah config --label org.opencontainers.image.description="minimal dev image for curl" "$bdr"
+buildah config --label org.opencontainers.image.licenses="MIT" "$bdr"
 
 # commit image
-buildah commit "$ctr" "${image_name}" # --disable-compression false --squash --sign-by --tls-verify
+buildah commit "$bdr" "${image_name}" # --disable-compression false --squash --sign-by --tls-verify
