7ab5b2a6f6c13a53ff98a9f8e4ac87f53a0b5ca1
Viktor Szakats <commit@vsz.me>
2026-01-24 19:58:33 +0100

    test_image.sh: try running dev builds (debian/fedora via create_dev_image.sh)

--- tests/test_image.sh
+++ tests/test_image.sh
@@ -23,21 +23,26 @@ branch_or_tag=${2}
 ctr=$(buildah from "${dist}:${branch_or_tag}")
 ctrmnt=$(buildah mount "$ctr")
 
-# check file exists
-if [[ ! -f "$ctrmnt/usr/bin/curl" ]]; then
-  echo "/usr/bin/curl does not exist."
-fi
-if [[ ! -f "$ctrmnt/usr/lib/libcurl.so.4.8.0" ]]; then
-  echo "/usr/lib/libcurl.so.4.8.0 does not exist."
-fi
+if [[ "$dist" = *'-dev-'* ]]; then
+  # test running curl
+  buildah run "$ctr" /build/usr/local/bin/curl -V
+else
+  # check file exists
+  if [[ ! -f "$ctrmnt/usr/bin/curl" ]]; then
+    echo "/usr/bin/curl does not exist."
+  fi
+  if [[ ! -f "$ctrmnt/usr/lib/libcurl.so.4.8.0" ]]; then
+    echo "/usr/lib/libcurl.so.4.8.0 does not exist."
+  fi
 
-# check symlink exists and is not broken
-if [ ! -L "$ctrmnt/usr/lib/libcurl.so.4" ] && [ ! -e "$ctrmnt/usr/lib/libcurl.so.4" ]; then
-  echo "/usr/lib/libcurl.so.4 symlink does not exist or is broken."
-fi
-if [ ! -L "$ctrmnt/usr/lib/libcurl.so" ] && [ ! -e "$ctrmnt/usr/lib/libcurl.so"  ]; then
-  echo "/usr/lib/libcurl.so symlink does not exist or is broken."
-fi
+  # check symlink exists and is not broken
+  if [ ! -L "$ctrmnt/usr/lib/libcurl.so.4" ] && [ ! -e "$ctrmnt/usr/lib/libcurl.so.4" ]; then
+    echo "/usr/lib/libcurl.so.4 symlink does not exist or is broken."
+  fi
+  if [ ! -L "$ctrmnt/usr/lib/libcurl.so" ] && [ ! -e "$ctrmnt/usr/lib/libcurl.so"  ]; then
+    echo "/usr/lib/libcurl.so symlink does not exist or is broken."
+  fi
 
-# test running curl
-buildah run "$ctr" /usr/bin/curl -V
+  # test running curl
+  buildah run "$ctr" /usr/bin/curl -V
+fi
