872371a9ffe726de79f51172f030a6096c7749a8
Viktor Szakats <commit@vsz.me>
2026-01-25 02:55:15 +0100

    try sth 1

--- create_dev_image.sh
+++ create_dev_image.sh
@@ -82,15 +82,15 @@ fi
 buildah run "$bdr" autoreconf -fi
 # shellcheck disable=SC2086
 time buildah run "$bdr" ./configure --disable-dependency-tracking ${build_opts}
-time buildah run "$bdr" make "-j$(nproc)"
+time buildah run "$bdr" make -j5
 
 # run tests
 if [[ $run_tests -eq 1 ]]; then
-  buildah run "$bdr" make test
+  buildah run "$bdr" make -j5 test
 fi
 
 # install curl in /build
-buildah run "$bdr" make DESTDIR="/build" install "-j$(nproc)"
+buildah run "$bdr" make -j5 install DESTDIR="/build"
 
 # label image
 buildah config --label org.opencontainers.image.source="https://github.com/curl/curl-container" "$bdr"
