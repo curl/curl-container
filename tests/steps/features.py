4aca0b0eebc58d684ba00b5a45a790086a50296d
Viktor Szakats <commit@vsz.me>
2026-09-01 18:01:19 +0200

    GHA: introduce ruff and shellcheck, reture CodeQL

--- tests/steps/features.py
+++ tests/steps/features.py
@@ -2,21 +2,22 @@ import re
 
 from behave import given
 
+
 def cleanup_output(data):
-    """strip output of ansi esc sequences (eg. color codes)"""
+    """strip output of ansi esc sequences (e.g. color codes)."""
     ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
     return ansi_escape.sub("", data)
 
-@given(u'running > podman run -it {image} curl -V')
+@given('running > podman run -it {image} curl -V')
 def invoke_podman_image_vflag(context, image):
     import subprocess
     cmd = ["podman", "run", "-it", image, "curl", "-V"]
-    p = subprocess.run(cmd, capture_output=True, text=True)
+    p = subprocess.run(cmd, capture_output=True, text=True, check=False)
     assert p.returncode == 0
 
-@given(u'running > podman run -it {image} -V')
+@given('running > podman run -it {image} -V')
 def invoke_podman_image(context, image):
     import subprocess
     cmd = ["podman", "run", "-it", image, "-V"]
-    p = subprocess.run(cmd, capture_output=True, text=True)
+    p = subprocess.run(cmd, capture_output=True, text=True, check=False)
     assert p.returncode == 0
